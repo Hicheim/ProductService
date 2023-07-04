436a1fa9be517e6f7ceae51673eab8ab04439c0d
Hicheim <hichame.chaalel@gmail.com>
2023-07-04 15:47:00 +0100

    Classe test sur le ProductController

---
+++ src/test/java/com/shopping/product/service/ProductServiceTest.java
@@ -0,0 +1,27 @@
+package com.shopping.product.service;
+
+import org.junit.jupiter.api.Test;
+
+
+class ProductServiceTest {
+
+    @Test
+    void createProduct() {
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void deleteProductById() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
